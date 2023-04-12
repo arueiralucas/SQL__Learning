c13f8d0ec48c60355c5b0d68de9331d933c02110
Lucas Ribeiro <108464429+arueiralucas@users.noreply.github.com>
2023-04-12 07:24:45 -0300

    beecrowd sql

---
+++ 2611.sql
@@ -0,0 +1,45 @@
+--- URI Online Judge SQL
+--- Copyright URI Online Judge
+--- www.urionlinejudge.com.br
+--- Problem 2611
+
+CREATE TABLE genres (
+  id numeric PRIMARY KEY,
+  description varchar(50)
+);
+
+CREATE TABLE movies (
+  id numeric PRIMARY KEY,
+  name varchar(50),
+  id_genres numeric REFERENCES genres (id)
+);
+
+INSERT INTO genres (id, description)
+VALUES
+  (1,	'Animation'),
+  (2,	'Horror'),
+  (3,	'Action'),
+  (4,	'Drama'),
+  (5,	'Comedy');
+  
+INSERT INTO movies (id, name, id_genres)
+VALUES
+  (1,	'Batman',	3),
+  (2,	'The Battle of the Dark River',	3),
+  (3,	'White Duck',	1),
+  (4,	'Breaking Barriers',	4),
+  (5,	'The Two Hours',	2);
+
+/*  Execute this query to drop the tables */
+-- DROP TABLE movies, genres; --
+/*
+Uma Vídeo locadora contratou seus serviços para catalogar os filmes dela. Eles precisam que você selecione o código e o nome dos filmes cuja descrição do gênero seja 'Action'.
+*/
+
+/*
+SELECT movies.id , movies.name
+FROM movies inner join genres 
+ON movies.id_genres = genres.id
+Where genres.description = 'Action';
+*/
+
